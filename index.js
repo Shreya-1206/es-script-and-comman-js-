f90aaf52c09bb5f825e036b651406fdb5cb958e0
Shreya <shreyaray.1206@gmail.com>
2024-05-07 19:11:29 +0530

    es script and comman js example

---
+++ index.js
@@ -0,0 +1,5 @@
+import {diff} from './math.js'
+// const math = require('./math');   //comman js
+
+// console.log(math.addNum(2,3));   //comman js 
+console.log(diff(232, 32));
---
+++ math.js
@@ -0,0 +1,7 @@
+// module.exports.addNum = function add(a,b){
+//     return a + b;
+// }
+
+export function diff(a,b){
+    return a - b;
+}
