352d6fdb2863c216120bdba2901954bfe2f0cf97
Shreya <shreyaray.1206@gmail.com>
2024-05-07 21:49:16 +0530

    es script and comman js example

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Used comman js and EsScript for importing the modules 💥
 ## used namespace 😄
+## used npm init command for setting up and initializing new Node.js projects, making it easier to manage project dependencies and configurations. 🚀
+## Installed os the core module of node
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import os from 'os';
 import * as Math from './math.js'
 // const math = require('./math');   //comman js
 
 // console.log(math.addNum(2,3));   //comman js 
 console.log(Math.diff(232, 32));
 console.log(Math.sum(23, 78));   // namespace used here
+
+console.log(os.version());
